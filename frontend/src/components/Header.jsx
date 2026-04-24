768d78be42c0c46759a680f6d68f8ca3d0bbcbaf
Gohel Bhargav <gohelbhargav401@gmail.com>
2026-04-25 01:32:09 +0530

    add: mobile view added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,15 @@
-import { FileText } from "lucide-react";
+import { FileText, HistoryIcon, HomeIcon, MenuIcon } from "lucide-react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Header() {
+
+  const [isOpen,setIsOpen] = useState(false)
+
+  // open manu handler
+  const menuHandler = () => {
+    setIsOpen(prev => !prev)
+  }
   return (
     <nav className="w-full border-b border-gray-200 bg-white/80 backdrop-blur-md sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -30,7 +38,24 @@ export default function Header() {
               History
             </NavLink>
           </div>
-
+          <div className="flex-col">
+          <div className={`text-violet-500 p-2 md:hidden ${isOpen && "mt-2 duration-500 transition-all" }`}>
+              <MenuIcon onClick={menuHandler}/>
+          </div>
+          <div className={`${isOpen && "flex gap-1 transition-all duration-700"}`}>
+                  { isOpen && (
+                      <>
+                        <NavLink to="/" className={`px-1 py-2 rounded-lg bg-violet-600 text-white text-sm font-medium hover:bg-violet-700 transition ${ isOpen && "transition duration-200" }`}>
+                            <HomeIcon />
+                          </NavLink>
+                          
+                          <NavLink to="/user/history" className={`px-1 py-2 rounded-lg bg-violet-600 text-white text-sm font-medium hover:bg-violet-700 transition ${ isOpen && "transition duration-200" }`}>
+                            <HistoryIcon />
+                          </NavLink>
+                      </>
+                  ) }
+              </div>
+</div>
         </div>
       </div>
     </nav>
