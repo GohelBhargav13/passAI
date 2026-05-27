eced6be7e4118adaff20a0f686916e21df2b10e7
Gohel Bhargav <gohelbhargav401@gmail.com>
2026-05-28 00:15:04 +0530

    fix: new redis cloud url

--- src/services/redis_services/redis_object.py
+++ src/services/redis_services/redis_object.py
@@ -1,14 +1,19 @@
+import os
 from redis import Redis
+from dotenv import load_dotenv
+from src.utills.apierror import ApiError
+
+load_dotenv()
+
+redis_cloud_url = os.getenv("REDIS_CLOUD_URL")
+
+if not redis_cloud_url:
+    raise ApiError(500,"Cloud URL is not found")
 
 def redis_create_object():
     return Redis(
-        host="localhost",
+        host=redis_cloud_url,
         port=6379,
         decode_responses=True,
         retry_on_timeout=True
     )
-
-
-
-
-
