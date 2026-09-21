369eb902675c66e25a62124dd8e458a5209e1489
Gohel Bhargav <gohelbhargav401@gmail.com>
2026-09-21 17:01:40 +0530

    fix: new model update CORS policy

--- src/__init__.py
+++ src/__init__.py
@@ -12,7 +12,7 @@ def create_app():
     CORS(app,resources=
         {
             r"/api/v1/*":{
-                      "origins":["https://pass-ai-pro.vercel.app","http://localhost:5173"],
+                      "origins":["https://pass-ai-pro.vercel.app","http://localhost:5173","http://localhost:5174"],
                       "methods":["GET","POST","DELETE","OPTIONS"]
                       } 
         },
--- src/services/llm_call.py
+++ src/services/llm_call.py
@@ -83,7 +83,7 @@ def call_llm_handler(header_data: str, paper_questions: str, user_prompt: str):
                     "content": prompt
                 }
             ],
-            model="llama-3.3-70b-versatile", 
+            model="llama-prompt-guard-2-86m", 
             temperature=0.2,
             max_tokens=6000                   
         )
